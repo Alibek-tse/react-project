8b13eeafc8dce6ed79a37c6b780e398b90656ae3
Alibek-tse <tokanov.a.k@gmail.com>
2020-12-23 17:37:27 +0600

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import { Card } from "../components/Card";
 import { Search } from "../components/Search";
+import { GithubContext } from "../context/github/githubContext";
 
 export const Home = () => {
-
-  const cards = new Array(15)
-    .fill('')
-    .map((_,i)=>i)
-
+  const {loading, users} = useContext(GithubContext)
+  
   return (
     <Fragment>
       <Search></Search>
 
       <div className="row">
-        {cards.map(card => {
-          return (
-            <div className="col-sm-4 mb-4" key={card}>
-            <Card></Card>
-          </div>
-          )
-        })}
+
+        {loading
+          ? <p className="text-center">Загрузка...</p>
+          : users.map(user => {
+            return (
+              <div className="col-sm-4 mb-4" key={user.id}>
+              <Card user={user}></Card>
+            </div>
+            )
+          })
+        }
+
 
         {/* <div className="col-sm-4 mb-4">
           <Card></Card>
