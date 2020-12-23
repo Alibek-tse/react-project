813686ac3952e04a6d37ac1c686707abbaf480bb
Alibek-tse <tokanov.a.k@gmail.com>
2020-12-23 11:14:42 +0600

    add search component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Card } from "../components/Card";
+import { Search } from "../components/Search";
 
 export const Home = () => {
+
+  const cards = new Array(15)
+    .fill('')
+    .map((_,i)=>i)
+    console.log(cards)
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Fragment>
+      <Search></Search>
+
+      <div className="row">
+        {cards.map(card => {
+          return (
+            <div className="col-sm-4 mb-4" key={card}>
+            <Card></Card>
+          </div>
+          )
+        })}
+
+        {/* <div className="col-sm-4 mb-4">
+          <Card></Card>
+        </div>
+
+        <div className="col-sm-4 mb-4">
+          <Card></Card>
+        </div>
+
+        <div className="col-sm-4 mb-4">
+          <Card></Card>
+        </div>  */}
+      </div>
+    </Fragment>
   );
 };
