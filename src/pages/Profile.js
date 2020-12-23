9b89e4163b7d41eaa3b05da6c4274c93809a56f1
Alibek-tse <tokanov.a.k@gmail.com>
2020-12-23 17:50:03 +0600

    fixed profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React,{useContext, useEffect, Fragment} from "react";
+import { GithubContext } from "../context/github/githubContext";
 
-export const Profile = () => {
+export const Profile = ({match}) => {
+  const github = useContext(GithubContext)
+  const name = match.params.name 
+  
+  useEffect(()=>{
+    // github.getUser(name)
+    // github.getRepos(name)
+    console.log('effect')
+  },[])
+
+  if (github.loading) {
+    return <p className="text-center">Загрузка...</p>
+  }
   return (
-    <div>
+    <Fragment>
       <h1>Profile Page</h1>
-    </div>
+    </Fragment>
   );
 };
