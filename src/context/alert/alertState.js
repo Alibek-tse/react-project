c3bce1c8029b0f3c68dd87dcbcb8256aced55851
Alibek-tse <tokanov.a.k@gmail.com>
2020-12-23 12:18:41 +0600

    reducer

--- src/context/alert/alertState.js
+++ src/context/alert/alertState.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React,{useReducer} from "react";
+import { SHOW_ALERT } from "../types";
+import { HIDE_ALERT } from "../types";
 import { AlertContext } from "./alertContext";
+import { alertReducer } from "./alertReducer";
+
+
 
 export const AlertState = ({ children }) => {
+  const [state, dispatch] = useReducer(alertReducer, null)
+
+  const hide = () => dispatch({type: HIDE_ALERT}) 
+
+  const show = (text, type = 'secondary') => {
+    dispatch({
+      type: SHOW_ALERT,
+      payload: {type, text}
+    })
+  }
+
   return (
-  <AlertContext.Provider>
+  <AlertContext.Provider value={{
+    hide, show, alert: state
+  }}>
     {children}
   </AlertContext.Provider>
   )
