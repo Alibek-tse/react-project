22c1f2d1378a16cb2816fab7fe3be201d22f7939
Alibek-tse <tokanov.a.k@gmail.com>
2020-12-24 10:18:37 +0600

    profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,22 +1,94 @@
-import React,{useContext, useEffect, Fragment} from "react";
+import React, { useContext, useEffect, Fragment } from "react";
 import { GithubContext } from "../context/github/githubContext";
+import { Link } from "react-router-dom";
 
-export const Profile = ({match}) => {
-  const github = useContext(GithubContext)
-  const name = match.params.name 
-  
-  useEffect(()=>{
-    // github.getUser(name)
-    // github.getRepos(name)
-    console.log('effect')
-  },[])
+export const Profile = ({ match }) => {
+  const github = useContext(GithubContext);
+  const urlName = match.params.name;
+
+  useEffect(() => {
+    github.getUser(urlName);
+    github.getRepos(urlName);
+    //eslint-disable-next-line
+  }, []);
 
   if (github.loading) {
-    return <p className="text-center">Загрузка...</p>
+    return <p className="text-center">Загрузка...</p>;
   }
+
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    public_repos,
+    public_gists,
+    following,
+  } = github.user;
   return (
     <Fragment>
-      <h1>Profile Page</h1>
+      <Link to="/" className="btn btn-link">
+        На главную
+      </Link>
+
+      <div className="card mb-4">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-3 text-center">
+              <img 
+                src={avatar_url} 
+                alt={name} 
+                style={{ width: "150px" }}
+                rel="noopener noreferrer"
+                ></img>
+              <h1>{name}</h1>
+              {location && <p>Местоположение: {location}</p>}
+            </div>
+            <div className="col">
+              {bio && (
+                <Fragment>
+                  <h3>BIO</h3>
+                  <p>{bio}</p>
+                </Fragment>
+              )}
+              <a href={html_url} target="_blank" className="btn btn-dark">
+                Открыть профиль
+              </a>
+              <ul>
+                {login && (
+                  <li>
+                    <strong>Username: </strong> {login}
+                  </li>
+                )}
+
+                {company && (
+                  <li>
+                    <strong>Компания: </strong> {company}
+                  </li>
+                )}
+
+                {blog && (
+                  <li>
+                    <strong>Website: </strong> {blog}
+                  </li>
+                )}
+              </ul>
+              <div className="badge badge-primary">Подписчики: {followers}</div>
+              <div className="badge badge-success">Подписан: {following}</div>
+              <div className="badge badge-info">
+                Репозитории: {public_repos}
+              </div>
+              <div className="badge badge-dark">Gists: {public_gists}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {github.repos.join()}
     </Fragment>
   );
 };
